a2cb958725f1866f0ea70db21b1d9d3542972125
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-10-23 19:36:57 -0700

    Updating documents in a collection

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@
 def update_topics(mongo_collection, name, topics):
     '''updating documents in a collection
     '''
-    mongo_collection.insert_many(
+    mongo_collection.update_many(
             {'name': name},
             {'$set': {'topics': topics}}
             )
